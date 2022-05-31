ed59a7b7afa92f17d08968dac80a7b73b0595ddb
AmirKasra_MomenKashani <71598934+AmirKasraKashani@users.noreply.github.com>
2022-05-31 22:07:20 +0430

    Update client.lua

--- client.lua
+++ client.lua
@@ -26,7 +26,7 @@ AddEventHandler("3dme:me", function(text, source, icon)
                         htmlString ..
                         '<span style="position: absolute; left: ' ..
                         xxx * 100 ..
-                        "%;top: " .. yyy * 100 .. '%;"><div class="me-container"><div class="icon-container"><span style="color:#cb73e6;"><i class="fas fa-'..icon..' fa-lg  "></i></span></div><div class="text-container"><b></b>' ..id .. text .. "</div></div></span>"
+"%;top: " .. yyy * 100 .. '%;"><div class="me-container"><div class="icon-container"><span style="color:#cb73e6;"><i class="fas fa-'..icon..' fa-lg  "></i></span></div><div class="text-container"><b>'..id..'</b>' .. text .. "</div></div></span>"
 --                 end
                 if lasthtmlString ~= htmlString then
                             SendNUIMessage({
