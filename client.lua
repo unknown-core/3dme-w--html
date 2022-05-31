ee5eac98055884105c9371e6337afe6280468e9f
AmirKasra_MomenKashani <71598934+AmirKasraKashani@users.noreply.github.com>
2022-05-31 23:43:02 +0430

    Update client.lua

--- client.lua
+++ client.lua
@@ -39,15 +39,16 @@ Citizen.CreateThread(function()
             local targetPed = GetPlayerPed(id)
             if targetPed ~= PlayerPedId() then
                 if playerDistances[id] then
-                    if playerDistances[id] < disPlayerNames then
+		    local iPed = GetPlayerPed(id)
+		    local lPed = PlayerPedId()
+                    if playerDistances[id] < disPlayerNames and HasEntityClearLosToEntity(lPed, iPed, 17) then
                         local targetPedCords = GetEntityCoords(targetPed)
-			local iPed = GetPlayerPed(id)
-			local lPed = PlayerPedId()
+
                         if NetworkIsPlayerTalking(id) then
                             DrawText3D(targetPedCords, GetPlayerServerId(id), 247,124,24)
                             DrawMarker(27, targetPedCords.x, targetPedCords.y, targetPedCords.z-0.97, 0, 0, 0, 0, 0, 0, 1.001, 1.0001, 0.5001, 173, 216, 230, 100, 0, 0, 0, 0)
                         else
-			    print(HasEntityClearLosToEntity(lPed, iPed, 17))
+-- 			    print(HasEntityClearLosToEntity(lPed, iPed, 17))
                             DrawText3D(targetPedCords, GetPlayerServerId(id), 255,255,255)
                         end
                     end
