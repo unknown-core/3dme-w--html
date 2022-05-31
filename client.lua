e2802ec1d00d45457c47f6f10038498936758d60
AmirKasra_MomenKashani <71598934+AmirKasraKashani@users.noreply.github.com>
2022-05-31 22:09:58 +0430

    Update client.lua

--- client.lua
+++ client.lua
@@ -16,7 +16,7 @@ AddEventHandler("3dme:me", function(text, source, icon)
                 local sourceCoords = GetEntityCoords(GetPlayerPed(playerId))
                 local nearCoords = GetEntityCoords(PlayerPedId())
                 local distance = Vdist2(sourceCoords, nearCoords)
---                 if distance < 50 then
+                if distance < 10 then
                     local onScreen, xxx, yyy =
                         GetHudScreenPositionFromWorldPosition(
                             sourceCoords.x + Config.CoordsX,
@@ -27,7 +27,7 @@ AddEventHandler("3dme:me", function(text, source, icon)
                         '<span style="position: absolute; left: ' ..
                         xxx * 100 ..
 "%;top: " .. yyy * 100 .. '%;"><div class="me-container"><div class="icon-container"><span style="color:#cb73e6;"><i class="fas fa-'..icon..' fa-lg  "></i></span></div><div class="text-container"><b>'..id..': </b>' .. text .. "</div></div></span>"
---                 end
+                end
                 if lasthtmlString ~= htmlString then
                             SendNUIMessage({
                                 toggle = true,
