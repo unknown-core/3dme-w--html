3083245a787c99728677e346e1e090fcc47c36de
AmirKasra_MomenKashani <71598934+AmirKasraKashani@users.noreply.github.com>
2022-05-31 22:54:34 +0430

    Update client.lua

--- client.lua
+++ client.lua
@@ -11,6 +11,7 @@ local function DrawText3D(position, text, r,g,b)
     local scale = scale*fov
     local ped = GetPlayerPed(i)
     if onScreen  then
+	 print(HasEntityClearLosToEntity(PlayerPedId(), ped, 17))
 --         if not useCustomScale then
             SetTextScale(0.0*scale, scale)
 --         else 
